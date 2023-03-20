4c9c7d9e1782f273106a237ad398076fd812d58b
Vinicius Maclters <vinicius.maclters@hotmail.com>
2023-03-20 10:59:07 -0300

    Iniciando projeto

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { Header } from "./components/Header";
+
+export function App() {
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,45 @@
+import styles from '../Header/styles.module.css'
+
+export function Header() {
+  return (
+    <nav>
+      <div>
+        <img src="" alt="" />
+      </div>
+
+      <ul>
+        <li>
+          <a href="">Home</a>
+        </li>
+
+        <li>
+          <a href="">Sobre</a>
+        </li>
+
+        <li>
+          <a href="">Modelos</a>
+        </li>
+
+        <li>
+          <a href="">Depoimentos</a>
+        </li>
+
+        <li>
+          <a href="">Nosso Time</a>
+        </li>
+
+        <li>
+          <a href="">Contato</a>
+        </li>
+      </ul>
+
+      <div>
+        <a href="">Entrar</a>
+        <a href="">Registrar</a>
+      </div>
+    </nav>
+
+
+
+  )
+}
